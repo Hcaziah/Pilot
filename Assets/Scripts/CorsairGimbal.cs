7b4eb68f57991b8cddc05a6e3744e61a1ead2693
DinosWithTacos <esteppzachary@live.com>
2017-01-27 17:15:13 -0500

    Fixed Camera Lock
    Finially :)

---
+++ Assets/Scripts/CorsairGimbal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorsairGimbal : MonoBehaviour {
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		/*double thisPositionY = transform.position.y;
+		double thisPositionX = transform.position.x;
+		double thisPositionZ = transform.position.z;
+		double thisRotation = transform.rotation.x;
+		thisPositionY = 4.5;
+		thisPositionX = 
+		thisPositionZ = 
+		thisRotation = 0;*/
+	}
+}
--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -9,7 +9,7 @@ public class MoveScript : MonoBehaviour {
 	void Start () {
 
 	}
-    public GameObject Corsair;
+    public GameObject MainCam1;
     public int rotating = 0;
 	public float speed = 6;
 	public float speedH;
@@ -19,15 +19,18 @@ public class MoveScript : MonoBehaviour {
 		}
 		if (Input.GetAxis ("Horizontal") < 0) {
 			transform.Translate (-1f * Time.deltaTime * (Input.GetAxis ("Horizontal") * Input.GetAxis ("Horizontal") * speed), 0f, 0f); //Backward
+            MainCam1.transform.Translate (-1f * Time.deltaTime * (Input.GetAxis ("Horizontal") * Input.GetAxis ("Horizontal") * speed), 0f, 0f, Space.World); // Camera Backwards
 		} 
 		if (Input.GetAxis ("Horizontal") > 0) {
 			transform.Translate (1f * Time.deltaTime * (Input.GetAxis ("Horizontal") * Input.GetAxis ("Horizontal") * speed), 0f, 0f); //Forward
+            MainCam1.transform.Translate (1f * Time.deltaTime * (Input.GetAxis ("Horizontal") * Input.GetAxis ("Horizontal") * speed), 0f, 0f, Space.World); //Forward
 		} 
 		if (Input.GetAxis ("Vertical") < 0) {
             transform.Rotate((-30f * Time.deltaTime * -Input.GetAxis("Vertical")), 0f ,0f);
            if (transform.eulerAngles.x < 345 || transform.eulerAngles.x == 0)
             {
                 transform.Translate (0f, 0f, -1f * Time.deltaTime * (Input.GetAxis("Vertical") * Input.GetAxis("Vertical") * speed), Space.World); //Right
+                MainCam1.transform.Translate (0f, 0f, -1f * Time.deltaTime * (Input.GetAxis("Vertical") * Input.GetAxis("Vertical") * speed), Space.World); //Right
                 transform.Rotate((30f * Time.deltaTime * -Input.GetAxis("Vertical")), 0f ,0f);
             }
 		}
@@ -36,13 +39,20 @@ public class MoveScript : MonoBehaviour {
             if (transform.eulerAngles.x > 15 || transform.eulerAngles.x == 0)
             {
                 transform.Translate (0f, 0f, 1f * Time.deltaTime * (Input.GetAxis("Vertical") * Input.GetAxis("Vertical") * speed), Space.World); //Left
-                transform.Rotate((-30f * Time.deltaTime * -Input.GetAxis("Vertical")), 0f ,0f);
+                MainCam1.transform.Translate (0f, 0f, 1f * Time.deltaTime * (Input.GetAxis("Vertical") * Input.GetAxis("Vertical") * speed), Space.World); //Left
+                transform.Rotate((-30f * Time.deltaTime * Input.GetAxis("Vertical")), 0f ,0f);
             }
             
         }
         if (Input.GetKeyDown ("q")) {
+            Quaternion MainCamRot =  MainCam1.transform.rotation;
             transform.position = new Vector3(0, 5, 0);
             transform.rotation = new Quaternion(0, 0, 0, 0);
+            MainCam1.transform.position = new Vector3(-7, 9, -7);
+            MainCamRot.x = 20;
+            MainCamRot.y = 45;
+            MainCamRot.z = 0;
+
         }
 	}
     IEnumerator Sleep()
